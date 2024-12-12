2427aff0167c08f899fb113bbd1f69f6bc4510af
Alaaddin74 <48843837+Alaaddin74@users.noreply.github.com>
2024-12-12 09:52:13 +0700

    Breeze

--- resources/views/list_user.blade.php
+++ resources/views/list_user.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
+<nav class="bg-white border-gray-200">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
           {{-- <img src="#" class="h-8" alt="Flowbite Logo" /> --}}
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">List of Users</span>
+          <span class="self-center text-2xl font-semibold whitespace-nowrap">List of Users</span>
       </a>
       <a href="{{ route('user.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3 inline-block">Tambah Pengguna Baru</a>
     </div>
       </nav>
     {{-- <h1 >List of Users</h1> --}}
     <div class="relative overflow-x-auto">
-    <table border="1" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table border="1" class="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
                 <th scope="col" class="px-6 py-3">ID</th>
                 <th scope="col" class="px-6 py-3">Nama</th>
@@ -24,16 +24,16 @@
         </thead>
         <tbody>
             @foreach ($kelas as $user)
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->id }}</td>
+                <tr class="bg-white border-b">
+                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ $user->id }}</td>
                     <td class="px-6 py-4">{{ $user->nama }}</td>
                     <td class="px-6 py-4">{{ $user->npm }}</td>
                     <td class="px-6 py-4">{{ $user->nama_kelas }}</td>
                     <td>
                         <!-- You can add actions like edit or delete here -->
-                        <a href="{{route('users.show', $user->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <a href="{{route('users.show', $user->id)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
                             Edit</a>
-                        <a href="#" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
+                        <a href="#" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">
                             Delete</a>
                     </td>
                 </tr>
